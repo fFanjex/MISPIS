78533ba39a453eb91f286bafaeb24421d31eeae8
Denis Ushakov <dens05.04@mail.ru>
2024-12-24 13:05:41 +0300

    Initial commit

---
+++ src/main/java/ru/fFanjex/AdminEmployee.java
@@ -0,0 +1,33 @@
+package ru.fFanjex;
+
+public class AdminEmployee extends Employee {
+
+    /**
+     *
+     * @param name
+     * @param salary
+     * @param email
+     * @param counter
+     * @param ssNo
+     */
+    public AdminEmployee(String name, double salary, String email, int counter, int ssNo) {
+        this.name = name;
+        this.email = email;
+        this.counter = counter;
+        this.ssNo = ssNo;
+    }
+
+    @Override
+    public String toString() {
+        return "src.AdminEmployee{" +
+                "counter=" + counter +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", ssNo=" + ssNo +
+                '}';
+    }
+
+    public void hireNewEmployee() {
+    }
+
+}
